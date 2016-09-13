39370369ea7cf4c650d912fd8116b4d86c81d8a7
Vahen <naeos@laposte.net>
2016-09-13 16:30:42 +0200

    Update
    * Amelioration de la construction de l'arbre des liens
    * Amelioration du main pour les entrées utilisateur
    * Clarifiction du main -> decoupage en plusieurs fonctions
    * Ajout du choix entre des fichiers avec extensions specifiques ou tout
    les fichiers sur le liens
    * Correction d'erreur dans les input
    * Ajout de commentaires
    * Traduction de commentaire de français à anglais

--- WebCrawlerUpdate.py
+++ WebCrawlerUpdate.py
@@ -16,39 +16,33 @@ import time
 dictLinks = {}
 domain = ""
 
-# Retire les doublons d'une liste
-def keepUnique(mylist):
-    return list(set(mylist))
-
 # Evite les erreurs de unicode
 # Bug parfois
 #FromRaw = lambda r: r if isinstance(r, unicode) else r.decode('utf-8', 'ignore')
 
+# Security checks for the link provided
 def securityCheck(baseLink,depth):
     global dictLinks
+    # Checks the depth we are at
     if depth <= 0:
         return False
-    # S'il y a trop de liens dans le dictionnaire
+    # Checks if there is to much links in the dictionnary
     if len(dictLinks) > 10000:
         print "Too much links in list -> stoping crawling"
         return False
-    # Teste si le lien donné est valide
+    # Checks if the provided link is correct
     if not linkCheck(baseLink):
         return False
-    if "?" in baseLink:
-        return False
-    # Test si le domaine du lien est valide
+    # Checks if the link is in the base domain
     if not has_domain(baseLink):
         return False
-    # Test si le lien est dans le dictionnaire 
+    # Checks if the link is already in the dictionnary and if has been visited
     if baseLink in dictLinks:
         if dictLinks[baseLink]:
             return False
-        else :
-            print baseLink
     return True
 
-# Construit la liste des liens telechargeable
+# Constructs the links dictionnary
 def constructTreeLink(baseLink,depth):
     if not securityCheck(baseLink,depth):
         return
@@ -60,45 +54,48 @@ def constructTreeLink(baseLink,depth):
     #read = FromRaw(read)
     soup = BeautifulSoup(read,"html.parser")
     links = soup.findAll("a")
-    
     for link in links:
         cleanString = link.get('href','/').replace("%20"," ")
         downloadLink = urlparse.urljoin(baseLink,cleanString)
-        if downloadLink not in dictLinks:
-            downloadLink = re.sub(r"[\t\n]","",downloadLink)
-            dictLinks[downloadLink] = False
-        constructTreeLink(downloadLink,depth-1)
-        dictLinks[downloadLink] = True
+        # Checks if we do not go back in the website
+        if not len(downloadLink) < len(baseLink):
+            # Avoid strange links
+            if not "?" in downloadLink:
+                if downloadLink not in dictLinks:
+                    downloadLink = re.sub(r"[\t\n]","",downloadLink)
+                    dictLinks[downloadLink] = False
+                    print downloadLink
+                constructTreeLink(downloadLink,depth-1)
+                dictLinks[downloadLink] = True
     return dictLinks
 
 
-# Telecharge tout ce qui match l'extension "extension dans la liste des fichiers
-def download_All_Update(links,extension):
+# Downloads only the files with the specific file extensions 
+def download_All_Update(links,extensions):
     links = set(links)
-    pattern_filename = re.compile('[^/,]+\.'+extension+'$')
     folder = "default"
-    print "######## Download START ########## "
-    for link in links:
-        name = pattern_filename.search(link)
-        if name:
-            m = re.search("http:\/\/(.*\/)",link)
-            if m:
-                folder = m.group(1)
-            create_folder(folder)                
-            print link
-            r = requests.get(link, stream=True)
-            with open(folder+"/"+name.group(0),"wb") as f:
-                for chunk in r:
-                    f.write(chunk)
-                    f.flush()
-    print "######## Download END   ########## "
+    for extension in extensions:    
+        pattern_filename = re.compile('[^/,]+\.'+extension+'$')
+        for link in links:
+            name = pattern_filename.search(link)
+            if name:
+                m = re.search("http:\/\/(.*\/)",link)
+                if m:
+                    folder = m.group(1)
+                create_folder(folder)                
+                print link
+                r = requests.get(link, stream=True)
+                with open(folder+"/"+name.group(0),"wb") as f:
+                    for chunk in r:
+                        f.write(chunk)
+                        f.flush()
 
-#Possible amelioration par rapport a la fonction au dessus
+
+# Downloads everything in the links provided
 def download_All_V2(links):
     links = set(links)
     pattern_filename = re.compile('(\w+)(\.\w+)+(?!.*(\w+)(\.\w+)+)$')
-    folder = "default"
-    print "######## Download START ########## "
+    folder = "default"    
     for link in links:
         name = link.split('/').pop()
         if pattern_filename.search(name):
@@ -112,9 +109,9 @@ def download_All_V2(links):
                 for chunk in r:
                     f.write(chunk)
                     f.flush()
-    print "######## Download END   ########## "
 
 
+# If a folder doesn't exist, it's created
 def create_folder(name):
     if not os.path.exists(name):
         print "Creating folder "+name                  
@@ -167,50 +164,100 @@ def linkCheck(link):
     u"$"
     , re.UNICODE),link)
 
+#Asks the user the base url he wants
+def askUrl():
+    baseurl = ""
+    while baseurl == "" or not linkCheck(baseurl):
+        baseurl = raw_input("Enter the URL : ")
+        if "http://" not in baseurl and "https://" not in baseurl :
+            baseurl = "http://"+baseurl
+    return baseurl
+
+#Asks the user if he wants files with specific extensions or everything  
+def askSpecific():
+    specificFiles = ""
+    extensions = []
+    endAddExt = ""
+    while (specificFiles != "y" and specificFiles != "n"):
+        specificFiles = raw_input("Enter :\ny -> if you wish to download files with specific extensions\nn -> download all files in the link\n")
+        if specificFiles == "y":
+            #If he wants specific file extensions, we ask for them
+            while True:
+                endAddExt = raw_input("Enter :\nA file extension you want to download(pdf,odp...whatever you want)\n/end -> Stop asking for extensions\n")
+                if endAddExt == "/end":
+                   break;
+                else :
+                   if len(endAddExt) == 3:
+                       extensions.append(endAddExt)
+                   else :
+                       print "File extensions must be only 3 caracters\n"
+            print "You asked for these extensions:"
+            print " | ".join(extensions)
+            if extensions == []:
+                print "List of extensions empty, switching to non specific mod"
+            break
+        elif specificFiles == "n":
+            break
+    return extensions
+
+#Asks the user for the maximum depth he wants to crawl too 
+def askDepth():
+    depth = ""
+    while True:
+        depth = raw_input("Enter the depth you wish to attain : ")
+        if not depth.isdigit():
+            continue
+        if int(depth) > 0:
+            break
+    return int(depth)
+
+#Asks the user if he wants to restart on another url or stop the program
+def askEnd():
+    end = ""
+    while (end !="y" and end !="n"):
+        end = raw_input("Do you wish to restart on another URL ? y/n\n")
+        if end == "y":
+            dictLinks = {}
+            return True              
+        elif end == "n":
+            print "Leaving program\n"
+            sys.exit(0)
+
+#Asks the user if he wants to start the crawling        
+def askStart():
+    wantStart = ""
+    while (wantStart !="y" and wantStart !="n" and wantStart !="r"):
+        wantStart = raw_input("Enter :\ny -> Start the crawler\nr -> Restart the program\nn -> Exit the program\n")
+        if wantStart == "n":
+            print "Leaving program"
+            sys.exit(0)
+        elif wantStart == "r":
+            print "Restarting program"
+    return wantStart == "y"
+
 
 if __name__ == '__main__':
     while True:
         global domain
         global dictLinks
-        start = False
-        while not start:
-            baseurl = ""
-            while baseurl == "" or not linkCheck(baseurl):
-                baseurl = raw_input("Enter the URL : ")
-                if "http://" not in baseurl and "https://" not in baseurl :
-                    baseurl = "http://"+baseurl
-                    
-            extension = ""
-            while extension == "":
-                extension = raw_input("Which type of files do you want ?( extensions , like pdf ,txt...whatever) : ")
-                
-            depth = -1
-            while (depth < 0 or not depth.isdigit()):
-                depth = raw_input("Enter the depth you want to crawl too : ")
+        while True:
+            baseurl = askUrl()
             domain = baseurl.split("/")[2]
-            print "Domain name is "+domain
-            wantStart = ""
-            while (wantStart !="y" and wantStart !="n" and wantStart !="r"):
-                wantStart = raw_input("Enter :\ny -> if you wish to start the crawler\nr -> to restart the program\nn -> Exit the program\n")
-                if wantStart == "y":
-                    start = True
-                elif wantStart == "n":
-                    print "Leaving program"
-                    sys.exit(0)
-                elif wantStart == "r":
-                    break
-                    continue
+            extensions = askSpecific()
+            depth = askDepth()
+            if askStart():
+                break
+            else :
+                continue
         print "######## Crawling START ##########"       
         t = constructTreeLink(baseurl,int(depth))
         print "######## Crawling END   ##########"
-        download_All_Update(t,extension)
-        end = ""
-        while (end !="y" and end !="n"):
-            end = raw_input("Do you wish to restart on another url ? y/n\n")
-            if end == "y":
-                dictLinks = {}
-                break                
-                continue
-            elif end == "n":
-                print "Leaving program\n"
-                sys.exit(0)
+        print "######## Download START ########## "
+        if extensions:
+            download_All_Update(t,extensions)
+        else :
+            download_All_V2(t);
+        print "######## Download END   ########## "
+        if askEnd():
+            continue
+        
